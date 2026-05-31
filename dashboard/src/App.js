cc613718f55dc90fd1a95872a7836444bc42af3c
swarnali2005 <swarnali2005ghosh@gmail.com>
2026-05-31 16:41:50 +0530

    [deploy] point dashboard to production API

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://icu-watch.onrender.com';
 
 function SeverityBadge({ severity }) {
   const colors = {
