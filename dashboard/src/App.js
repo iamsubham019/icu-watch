9b2c60303282812e85ee8f3507ae4157f2dd019c
swarnali2005 <swarnali2005ghosh@gmail.com>
2026-05-31 13:35:08 +0530

    [merge] Swarnali backend merged to main

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'http://127.0.0.1:8001';
 
 function SeverityBadge({ severity }) {
   const colors = {
