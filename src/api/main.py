1f06bbaa8dcdc654a0074baf354125de01ba1e76
swarnali2005 <swarnali2005ghosh@gmail.com>
2026-05-31 17:20:16 +0530

    [fix] add Vercel domain to CORS allowed origins

--- src/api/main.py
+++ src/api/main.py
@@ -38,7 +38,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:3001"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001", "https://icu-watch.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
